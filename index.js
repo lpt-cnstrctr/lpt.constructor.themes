f46cfed51299ea1ab465d9b30da583f0c4adc519
versus <dcversus@gmail.com>
2016-10-05 14:26:47 +0300

    update deps, use new directory structure

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ module.exports = dep => {
     require('postcss-import')({
       addDependencyTo: dep
     }),
-    require('postcss-url'),
-    require('postcss-for'),
     require('postcss-mixins'),
-    require('postcss-nested'),
     require('postcss-simple-vars'),
-    require('postcss-conditionals'),
     require('postcss-each'),
     require('cssnano')(),
     require('postcss-reporter')()
