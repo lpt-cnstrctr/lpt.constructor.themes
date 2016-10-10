799eade4d6c59c358d1790434bd924aa6bf47e48
versus <dcversus@gmail.com>
2016-10-10 13:52:35 +0300

    serve static

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const plugins = require('.');
+const serve = require('gulp-serve');
 
 gulp.task('css', () =>
   gulp
@@ -9,8 +10,11 @@ gulp.task('css', () =>
     .pipe(gulp.dest('./build'))
 );
 
+gulp.task('serve', serve(['build', '.']));
+
 gulp.task('watch', ['css'], () =>
   gulp.watch(['./src/**/*.css', './index.css'], ['css'])
 );
 
+gulp.task('start', ['watch', 'serve']);
 gulp.task('default', ['css']);
