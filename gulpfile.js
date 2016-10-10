5d75f0909cc29c9bdff94001ebd2aeb61633ec32
versus <dcversus@gmail.com>
2016-10-10 19:10:11 +0300

    github pages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const plugins = require('.');
 const serve = require('gulp-serve');
+const ghPages = require('gulp-gh-pages');
 
 gulp.task('css', () =>
   gulp
     .src('./index.css')
     .pipe(postcss(plugins()))
     .pipe(gulp.dest('./build'))
+);
+
+gulp.task('deploy', () =>
+  gulp
+    .src('build/**/*')
+    .pipe(ghPages())
 );
 
 gulp.task('serve', serve(['build']));
