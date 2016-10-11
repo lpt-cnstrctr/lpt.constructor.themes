2e86df2d07e8760a203b21414ce6946119744569
versus <dcversus@gmail.com>
2016-10-11 13:11:09 +0300

    kss as gulp task integration

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const postcss = require('gulp-postcss');
 const plugins = require('.');
 const serve = require('gulp-serve');
 const ghPages = require('gulp-gh-pages');
+const run = require('gulp-run');
 
 gulp.task('css', () =>
   gulp
@@ -17,17 +18,23 @@ gulp.task('fonts', () =>
     .pipe(gulp.dest('./build/src/fonts'))
 );
 
-gulp.task('deploy', () =>
+gulp.task('kss', () =>
+  run('npm run kss').exec()
+);
+
+gulp.task('deploy', ['build'], () =>
   gulp
     .src('build/**/*')
     .pipe(ghPages())
 );
 
+
 gulp.task('serve', serve(['build']));
 
-gulp.task('watch', ['default'], () =>
-  gulp.watch(['./src/**/*.css', './index.css'], ['css'])
+gulp.task('watch', ['build'], () =>
+  gulp.watch(['./src/**/*.css', './index.css'], ['css', 'kss'])
 );
 
+gulp.task('build', ['css', 'fonts', 'kss']);
 gulp.task('start', ['watch', 'serve']);
-gulp.task('default', ['css', 'fonts']);
+gulp.task('default', ['build']);
